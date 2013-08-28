aarizaq/inetmanet-3.x

commit 09bbf2f504cb9865314b6699857b0c345639b503

Indentify collisions and bit errors

src/wpan/phyLayer/ieee802154/Ieee802154RadioModel.cc
@@ -19,7 +19,7 @@ double Ieee802154RadioModel::calculateDuration(AirFrame *airframe)
 }
 
 
-bool Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList)
+PhyIndication Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList)
 {
     // calculate snirMin
     double snirMin = receivedList.begin()->snr;
@@ -33,12 +33,12 @@ bool Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList
     {
         // if snir is too low for the packet to be recognized
         EV << "COLLISION! Packet got lost\n";
-        return false;
+        return COLLISION;
     }
     if (!packetOk(snirMin, frame->getBitLength(), airframe->getBitrate()))
     {
     	EV << "Packet has BIT ERRORS! It is lost!\n";
-    	return false;
+    	return FRAMEOK;
     }
     /*else if (packetOk(snirMin, airframe->getEncapsulatedMsg()->length(), airframe->getBitrate()))
     {
@@ -51,7 +51,7 @@ bool Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList
         return false;
     }*/
 
-    return true;
+    return BITERROR;
 }
 
 

src/underTest/_4gsim/linklayer/lte/LTERadioModel.cc
@@ -57,7 +57,7 @@ double LTERadioModel::calculateDuration(AirFrame *airframe)
 }
 
 
-bool LTERadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList)
+PhyIndication LTERadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList)
 {
     // calculate snirMin
     double snirMin = receivedList.begin()->snr;
@@ -69,17 +69,17 @@ bool LTERadioModel::isReceivedCorrectly(AirFrame *airframe, const SnrList& recei
     {
         // if snir is too low for the packet to be recognized
         EV << "COLLISION! Packet got lost\n";
-        return false;
+        return COLLISION;
     }
     else if (isPacketOK(snirMin, airframe->getBitLength()+headerLengthBits, airframe->getBitrate()))
     {
         EV << "packet was received correctly, it is now handed to upper layer...\n";
-        return true;
+        return FRAMEOK;
     }
     else
     {
         EV << "Packet has BIT ERRORS! It is lost!\n";
-        return false;
+        return BITERROR;
     }
 }
 

src/underTest/_4gsim/linklayer/lte/LTERadioModel.h
@@ -39,7 +39,7 @@ class INET_API LTERadioModel : public IRadioModel
 
     virtual double calculateDuration(AirFrame *airframe);
 
-    virtual bool isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList);
+    virtual PhyIndication isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList);
     // used by the Airtime Link Metric computation
     virtual bool haveTestFrame() {return false;}
     virtual double calculateDurationTestFrame(AirFrame *airframe) {return 0;}

src/underTest/wimax/linklayer/radio/Ieee80216Radio.cc
@@ -132,10 +132,12 @@ void Ieee80216Radio::handleLowerMsgEnd(AirFrame *airframe)
         }
         else
         {
-            if (!radioModel->isReceivedCorrectly(airframe, list))
+
+            PhyIndication frameState = radioModel->isReceivedCorrectly(airframe, list);
+            if (frameState != FRAMEOK)
             {
-                airframe->getEncapsulatedPacket()->setKind(list.size() > 1 ? COLLISION : BITERROR);
-                airframe->setName(list.size() > 1 ? "COLLISION" : "BITERROR");
+                airframe->getEncapsulatedPacket()->setKind(frameState);
+                airframe->setName(frameState == COLLISION ? "COLLISION" : "BITERROR");
                 isCollision = true;
                 collisions++;
             }

src/wpan/phyLayer/ieee802154/Ieee802154Phy.cc
@@ -607,11 +607,13 @@ void Ieee802154Phy::handleLowerMsgEnd(AirFrame * airframe)
                 if (iter->snr < snirMin)
                     snirMin = iter->snr;
             airframe->setSnr(10*log10(snirMin));
-            if (!radioModel->isReceivedCorrectly(airframe, list))
+            PhyIndication frameInfo = radioModel->isReceivedCorrectly(airframe, list);
+            if (frameInfo != FRAMEOK)
             {
                 isCollision = true;
                 // we cannot do this before decapsulation, because we only detect this packet collided, not all, refer to encapsulation msg
                 //airframe->getEncapsulatedMsg()->setKind(COLLISION);
+
             }
 
             // we decapsulate here to set some flag
@@ -624,7 +626,7 @@ void Ieee802154Phy::handleLowerMsgEnd(AirFrame * airframe)
             frame->setControlInfo(cinfo);
 
             if (isCollision)
-                frame->setKind(COLLISION);
+                frame->setKind(frameInfo);
             else if (CCA_timer->isScheduled())  // during CCA, tell MAC layer to discard this pkt
                 frame->setKind(RX_DURING_CCA);
 

src/wpan/phyLayer/ieee802154/Ieee802154RadioModel.h
@@ -14,7 +14,7 @@ class INET_API Ieee802154RadioModel : public IRadioModel
 
     virtual double calculateDuration(AirFrame *airframe);
 
-    virtual bool isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList);
+    virtual PhyIndication isReceivedCorrectly(AirFrame *airframe, const SnrList& receivedList);
     // used by the Airtime Link Metric computation
     virtual bool haveTestFrame() {return false;}
     virtual double calculateDurationTestFrame(AirFrame *airframe) {return 0;}

michaelkirsche/inetmanet-2.0

commit 8d55dbaa64e26ea18d3e0f91e78a6a64b5af9290

Fix error that was introduced by commit https://github.com/aarizaq/inetmanet-2.0/commit/09bbf2f504cb9865314b6699857b0c345639b503

src/wpan/phyLayer/ieee802154/Ieee802154RadioModel.cc
@@ -38,7 +38,7 @@ PhyIndication Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, cons
     if (!packetOk(snirMin, frame->getBitLength(), airframe->getBitrate()))
     {
     	EV << "Packet has BIT ERRORS! It is lost!\n";
-    	return FRAMEOK;
+    	return BITERROR;
     }
     /*else if (packetOk(snirMin, airframe->getEncapsulatedMsg()->length(), airframe->getBitrate()))
     {
@@ -51,7 +51,7 @@ PhyIndication Ieee802154RadioModel::isReceivedCorrectly(AirFrame *airframe, cons
         return false;
     }*/
 
-    return BITERROR;
+    return FRAMEOK;
 }
 
 